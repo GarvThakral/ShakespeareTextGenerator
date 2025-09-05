69c5eab69df235095a5d5b15ccc1a63b1d94bf54
Garv Thakral <garvthakralfx@gmail.com>
2025-09-06 01:15:34 +0530

    Init repository

---
+++ steps/ingest_data.py
@@ -0,0 +1,22 @@
+from zenml import step
+import datasets as ds
+from datasets import load_dataset
+import re
+import logging
+from typing import Tuple , Union , Any
+from datasets import Dataset, DatasetDict, IterableDataset, IterableDatasetDict
+
+@step
+def ingest_data()->Tuple[Union[DatasetDict, Dataset, IterableDatasetDict, IterableDataset], int]:
+    logging.info("Loading the dataset from Datasets .....")
+    ds = load_dataset("Trelis/tiny-shakespeare")
+    logging.info("Dataset loaded !")
+    maxInDs = 0
+    logging.info("Calculating max length ......")
+    for x in ds['train']:
+        x:Any = x 
+        sentence_array = re.findall(r"\b\w+(?:'\w+)?\b|[.!?,-]", x['Text'].lower())
+        maxInDs = max(len(sentence_array),maxInDs)
+    return ds , maxInDs
+
+ingest_data()
---
+++ steps/preprocess_data.py
@@ -0,0 +1,46 @@
+from zenml import step
+import datasets as ds
+import logging 
+from typing import Tuple , Any , Dict
+import re
+
+def preprocessing_data(ds:ds.DatasetDict,maxInDs:int)->Tuple[ds.Dataset,ds.Dataset,int,Dict[int , str],Dict[str,int],int]:
+    newVocab = []
+
+    def create_new_vocab(sentence,newVocab):
+        sentence_array = re.findall(r"\b\w+(?:'\w+)?\b|[.!?,-]", sentence.lower())
+        new_words = [x for x in sentence_array if x not in newVocab]
+        new_words_set = set(new_words)
+        new_words = list(new_words_set)
+        newVocab += new_words
+
+    ds['train'].map(lambda x : create_new_vocab(x["Text"],newVocab))
+    ds['test'].map(lambda x : create_new_vocab(x["Text"],newVocab))
+
+    newVocab.insert(0,"<UNK>")
+    newVocab.insert(0,"<PAD>")
+    
+    vocab_size = len(newVocab)
+    index_to_word = {i:k for i,k in enumerate(newVocab)}
+    word_to_index = {k:i for i,k in enumerate(newVocab)}
+
+    def preprocessing_layer(sentence):
+        sentence_array = re.findall(r"\b\w+(?:'\w+)?\b|[.!?,-]", sentence.lower())
+        # print(sentence_array)
+        wor_to_in = [word_to_index[x] for x in sentence_array]
+        # print(wor_to_in)
+        pad_len = maxInDs - len(wor_to_in)
+        zero_array = [0]*pad_len
+        # print(len(wor_to_in))
+        padded_wor_to_in = wor_to_in+zero_array
+        # print(len(padded_wor_to_in))
+        if(len(padded_wor_to_in) > maxInDs):
+            logging.info("Sentence size mismatch")
+            return "STOP"
+        labels = padded_wor_to_in[1:] + [0]
+        return {"features":padded_wor_to_in,"labels":labels}
+
+    processed_ds_train = ds['train'].map(lambda x : preprocessing_layer(x["Text"]))
+    processed_ds_test = ds['test'].map(lambda x : preprocessing_layer(x["Text"]))
+
+    return processed_ds_train , processed_ds_test , maxInDs , index_to_word , word_to_index , vocab_size
